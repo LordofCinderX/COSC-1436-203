cfcf490746279a4a30f555dff15f35fba7dcc40e
LordofCinderX <tracerogers14@yahoo.com>
2025-06-09 15:12:51 +0000

    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+// This is the HelloWorld class
+public class HelloWorld {
+    // The main() method is the entry point of any Java application
+    public static void main(String [] args){
+        // Prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
+        // Prints your name
+        System.out.println("Clayton Rogers");
+        // Prints your favorite food
+        System.out.println("My favorite food is buffalo wings.");
+        // Prints a celebrity or public figure you'd like to meet
+        System.out.println("I would like to meet Chris Pratt.");
+        // Prints your least favorite chore at home
+        System.out.println("My least favorite chore is washing the dishes.");
+        // Prints what you'd rather be doing right now
+        System.out.println("I would rather be playing video games right now.");
+        
+    }
+}
+
+// The main() method is the entry point of a Java program.
+// System.out.println() is used to print messages to the console.
+// Comments are used to explain the code so that others can understand what the code does.
